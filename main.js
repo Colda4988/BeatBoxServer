7cb5ab239c2ace6f77386a664ae4b3bfd4c618ce
Colda4988 <guswnd8800@naver.com>
2021-09-29 00:43:49 +0900

    Socket IO

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-//test end
+var app = require('http').createServer(handler)
+var fs = require('fs');
+
+app.listen(4444);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+      if (err) {
+          res.writeHead(500);
+          return res.end('Error loading index.html');
+      }
+
+      res.writeHead(200);
+      res.end(data);
+  });
+}
+
+// socket.io 스타트
+var io = require('socket.io')(app);
+
+// 클라이언트 컨넥션 이벤트 처리
+io.on('connection', function (socket) {
+  console.log('Hello, socket.io~');
+});
