93a18c5ec1b8aec739f724074f7cea41aecc0b8d
Colda4988 <guswnd8800@naver.com>
2021-09-30 13:49:56 +0900

    d

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 var express = require('express')
+var fs = require('fs')
+
+var Mapfiles = fs.readdirSync(__dirname+"/maps");
+console.log(Mapfiles)
 
 // express 는 함수이므로, 반환값을 변수에 저장한다.
 var app = express()
@@ -12,5 +16,43 @@ app.listen(4444, function() {
 // localhost:3000 브라우저에 res.send() 내부의 문자열이 띄워진다.
 
 app.get('/', function(req,res) {
-  res.send("<h1>hi friend!</h1>")
+  res.send("<h1>hi friend!</h1>");
 })
+
+app.get('/beatmap', function(req,res) {
+  res.send("<h1>This is Beatmap Page</h1>");
+})
+
+app.get('/beatmap/:id', (req, res) => {
+
+  const mapid = Mapfiles.find(map => Number(map) === Number(req.params.id));
+
+  if (!mapid) {
+    return next(err);
+  }
+
+  res.send("<h1>This is Beatmap Page MapID : "+mapid+"</h1>");
+  console.log(mapid);
+});
+
+app.get('/beatmap/:id/download', (req, res) => {
+
+  const mapid = Mapfiles.find(map => Number(map) === Number(req.params.id));
+
+  if (!mapid) {                                  
+    return next(err);
+  }
+  res.download(__dirname+'/maps/'+mapid+'/map.bm');
+  
+  history.back();
+});
+
+app.get('*', (req,res)=>{
+	return next(err);
+}); 
+
+app.use(function(err, req, res, next) {
+   //console.error(err.stack);
+   res.status(404).send("해당페이지를 찾을 수 없습니다")
+   res.status(500).send('Something broke!'); 
+});
